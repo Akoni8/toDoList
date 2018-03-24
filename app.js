f08f5e6bee8e91bf14b94ddd46358c2b09794e57
Akoni <hakmorrison@csumb.edu>
2018-03-24 03:50:36 +0000

    added api for todos

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var app = express();
 var todoRoutes = require("./routes/todo");
+var bodyParser = require("body-parser");
+
+app.use('/api/todo', todoRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+
+
+
 app.get('/', function(req, res){
-    res.send("Hello from root");
+    res.sendFile("index.html");
 });
+
 
 
 
--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var db = require("../models");
+var helpers = require("../helpers/todo");
 
-router.get("/", function(req,res){
-    res.send("hello from todos")
-})
+router.route("/")
+    .get(helpers.getTodo)
+    .post(helpers.createTodo)
+
+router.route("/:todoId")
+    .get(helpers.showTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
+
+module.exports = router;
